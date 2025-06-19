c6c37e34a711b630dec9cc2c21784b2731dfdff2
Jonathan Guevara <jguevara@optimeconsulting.com>
2025-06-19 18:30:34 -0500

    feat: remove .github folder

--- .github/actions/test_tests-web.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e -u -x -o pipefail
-
-php -S localhost:8088 phpunit/router.php &>/dev/null &
-bin/console config:set --config-dir=./tests/config --context=inventory enabled_inventory 1
-vendor/bin/phpunit phpunit/web
-bin/console config:set --config-dir=./tests/config --context=inventory enabled_inventory 0
